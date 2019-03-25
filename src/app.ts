0a3538fbfe9185c72ce46a48831c839c161d0eb0
zstrnad <zbynek@gamingrealms.com>
2019-03-25 15:38:43 +0000

    initial load

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import compression from "compression";  // compresses requests;
+import { Request, Response } from "express";
+
+import compression from "compression"; // compresses requests;
 import bodyParser from "body-parser";
 import path from "path";
 import expressValidator from "express-validator";
 import {Home} from "./app/controllers/home.controller";
-
-// Controllers (route handlers)
 
 
 // Create Express server
@@ -17,19 +17,22 @@ app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
 
 
 app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+    express.static(path.join(__dirname, "public"), {maxAge: 31557600000})
 );
 
 /**
  * Primary app routes.
  */
-app.get("/", (res) => {
+app.get("/", (req: Request, res: Response) => {
     new Home().run();
+    req.render("home", {
+        title: "Home"
+    });
 });
 
 
